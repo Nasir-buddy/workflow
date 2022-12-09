e4817d8de32c7a3328a02b03b9cdb8502fc9eb10
nasir-buddy <syedali172000@gmail.com>
2022-12-09 10:20:08 +0530

    Fix bugs

--- 1D-Array-Programs/Remove Duplicates from an Array/Code.java
+++ 1D-Array-Programs/Remove Duplicates from an Array/Code.java
@@ -7,18 +7,21 @@ public class Code {
             temp.add(array[i]);
         }
         for(int i=0;i<temp.size();i++){
+
             for(int j=i+1;j<temp.size();j++){
                 if(temp.get(i)==temp.get(j)){
                     temp.remove(j);
                 }
             }
+            
         }
         for(int e: temp){
             System.out.print(e + " ");
         }
     }
     public static void main(String[] args) {
-        int test[] = {1,2,3,2,3,2,4,5,5,6,4,6,7};
+        int test[] = {1,2,3,2,3,2,4};
+
         RDuplicates(test);
     }    
 }
